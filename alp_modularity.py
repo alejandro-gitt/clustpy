3c6031ac5abbe8cb381d9a8232421f991aee088c
aleky29 <aleartpro@gmail.com>
2022-03-04 10:07:07 +0100

    vsc alp-mod

--- alp_modularity.py
+++ alp_modularity.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 from networkx.algorithms.community.community_utils import is_partition
+# from tabusearch import tabu_modularity_optimizationd
 
 G = nx.MultiDiGraph()
 weight = 'weight'
@@ -47,49 +48,28 @@ plt.show()
 
 weight = 'weight'
 
-if not isinstance(communities, list):
-    communities = list(communities)
-if not is_partition(G, communities):
-    print('notAPartition')
+# if not isinstance(communities, list):
+#     communities = list(communities)
+# if not is_partition(G, communities):
+#     print('notAPartition')
 
-pos_total_weight = sum(wt for u, v, wt in G.edges(data = weight, default=1) if wt > 0) #Sum all positive links
-neg_total_weight = sum(-wt for u, v, wt in G.edges(data=weight, default=1) if wt < 0) #Sum all negative links
+# pos_total_weight = sum(wt for u, v, wt in G.edges(data = weight, default=1) if wt > 0) #Sum all positive links
+# neg_total_weight = sum(-wt for u, v, wt in G.edges(data=weight, default=1) if wt < 0) #Sum all negative links
 
 
-def community_contribution_directed_negatively_weighted(community):
-    comm = set(community)
-    L_a = sum(wt for u, v, wt in G.edges(comm, data=weight, default=1) if v in comm)
-    w_out_positive_comm = sum(wt for u,v,wt in G.edges(community, data=weight, default=1) if wt > 0)
-    w_out_negative_comm = sum(-wt for u,v,wt in G.edges(community, data=weight, default=1) if wt < 0)
-    w_in_all_comm = [wt for u,v,wt in G.edges( data=weight, default=1) if v in community]
-    w_in_positive_comm = sum(wt for u,v,wt in G.edges( data=weight, default=1) if v in community and wt > 0)
-    w_in_negative_comm = sum(-wt for u,v,wt in G.edges( data=weight, default=1) if v in community and wt < 0)
+# def community_contribution_directed_negatively_weighted(community):
+#     comm = set(community)
+#     L_a = sum(wt for u, v, wt in G.edges(comm, data=weight, default=1) if v in comm)
+#     w_out_positive_comm = sum(wt for u,v,wt in G.edges(community, data=weight, default=1) if wt > 0)
+#     w_out_negative_comm = sum(-wt for u,v,wt in G.edges(community, data=weight, default=1) if wt < 0)
+#     w_in_all_comm = [wt for u,v,wt in G.edges( data=weight, default=1) if v in community]
+#     w_in_positive_comm = sum(wt for u,v,wt in G.edges( data=weight, default=1) if v in community and wt > 0)
+#     w_in_negative_comm = sum(-wt for u,v,wt in G.edges( data=weight, default=1) if v in community and wt < 0)
     
-    return L_a - (w_in_positive_comm*w_out_positive_comm)/(2*pos_total_weight) + (w_in_negative_comm*w_out_negative_comm)/(2*neg_total_weight)
+#     return L_a - (w_in_positive_comm*w_out_positive_comm)/(2*pos_total_weight) + (w_in_negative_comm*w_out_negative_comm)/(2*neg_total_weight)
 
 
-result  = (1/(2*(pos_total_weight + neg_total_weight)))*sum(map(community_contribution_directed_negatively_weighted, communities))
-print(result)
+# result  = (1/(2*(pos_total_weight + neg_total_weight)))*sum(map(community_contribution_directed_negatively_weighted, communities))
+# print(result)
 
-# edges_out_all_comm = G.edges(community, data=weight, default=1)
-# print('all weights OUT the community: ',edges_out_all_comm)
-# w_out_positive_comm = [wt for u,v,wt in edges_out_all_comm if wt > 0]
-# print('positive weights out: ',w_out_positive_comm)
-# w_out_negative_comm = [wt for u,v,wt in edges_out_all_comm if wt < 0]
-# print('negative weights out: ',w_out_negative_comm)
-
-# edges_all = G.edges(data = weight)
-# print('every edge on the Graph is: ',edges_all)
-
-# w_in_all_comm = [wt for u,v,wt in edges_all if v in community]
-# print('incident weights into the community are: ',w_in_all_comm)
-
-# w_in_positive_comm = [wt for u,v,wt in edges_all if v in community and wt > 0]
-# print('incident positive weights into the community are: ',w_in_positive_comm)
-
-# w_in_negative_comm = [wt for u,v,wt in edges_all if v in community and wt<0]
-# print('incident negative weights into the community are: ',w_in_negative_comm)
-
-# w_all_comm = sum(w_out_positive_comm) + sum(w_out_negative_comm) + sum(w_in_all_comm)
-
-# print('Lalpha = ',w_all_comm)
+# print(G.nodes)
