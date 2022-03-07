3d93e0d28fc90c054e7ebca53381170cad5df493
aleky29 <aleartpro@gmail.com>
2022-03-07 14:37:16 +0100

    buscando donde s_best falla

--- tabusearch.py
+++ tabusearch.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import random
 from mymod import mymodularity
 from random import randrange
 
@@ -38,62 +39,69 @@ def solution_from_move(network, s_iter, node):
     # Buscamos en este metodo mover un solo nodo, escogiendo aleatoriamente entre las opciones:
     # - Mover el nodo de una comunidad a otra (aleatoria)
     # - Crear una nueva comunidad e incluir el nodo en esta.(El nodo solo puede estar en una comunidad a la vez)
-    
-    
+    current_community_index = find_nodes_community(s_iter, node) #INDICE DE LA COMUNIDAD EN LA QUE SE ENCUENTRA EL NODO ACTUALMENTE
+
+    def create_new_community(communities,node):
+        print('Ha elegido crear una comunidad')
+        # Primero, creamos una nueva comunidad y añadimos nuestro nodo en ella
+        communities.append(frozenset({node}))
+        # Eliminamos el nodo de su comunidad origen
+        communities_setted = [set(community) for community in communities]
+        communities_setted[current_community_index].remove(node)
+        sol_with_new_community = [frozenset(community_setted) for community_setted in communities_setted]
+        print('quedan las siguientes comunidades:',sol_with_new_community)
+        return sol_with_new_community
+
     def change_to_random_community(communities, node):
         # Ahora, movemos node a uno aleatorio que no sea en el que se encuentra actualmente
-        
-        if len(communities) <= 1:
-            #Debemos crear una nueva comunidad en la que introducir el nodo
-            print('Solo existe una comunidad o ninguna, creamos una nueva que solo contenga al nodo')
-            communities.append(frozenset({node}))
-        else:
-            # Movemos el nodo de su comunidad actual a otra aleatoria
-            current_community_index = find_nodes_community(communities, node) #INDICE
+        # Movemos el nodo de su comunidad actual a otra aleatoria
+        dest_community_index = randrange(0,len(communities),1)
+        while(dest_community_index == current_community_index):
+            # print('coincide el random',dest_community_index,'ACTUAL:',current_community_index)
             dest_community_index = randrange(0,len(communities),1)
-            while(dest_community_index == current_community_index):
-                # print('coincide el random',dest_community_index,'ACTUAL:',current_community_index)
-                dest_community_index = randrange(0,len(communities),1)
-            
-            # print('nodo',node,'pretende ir de comunidad',current_community_index,'a',dest_community_index)
+        
+        # print('nodo',node,'pretende ir de comunidad',current_community_index,'a',dest_community_index)
 
-            #Añadimos el nodo a destination_community y lo eliminamos el original:
-            # print('comunidades originalmente',communities)
-            #Copiamos las comunidades en len(communities) 'sets' en lugar de frozensets para poder añadir y
-            # eliminar, aunque luego lo volveremos a convertir en frozenset
+        #Añadimos el nodo a destination_community y lo eliminamos el original:
+        # print('comunidades originalmente',communities)
+        #Copiamos las comunidades en len(communities) 'sets' en lugar de frozensets para poder añadir y
+        # eliminar, aunque luego lo volveremos a convertir en frozenset
 
-            communities_setted = [set(community) for community in communities]
-            communities_setted[dest_community_index].add(node)
-            communities_setted[current_community_index].remove(node)
-            new_random_partition = [frozenset(community_setted) for community_setted in communities_setted]
+        communities_setted = [set(community) for community in communities]
+        communities_setted[dest_community_index].add(node)
+        communities_setted[current_community_index].remove(node)
+        new_random_partition = [frozenset(community_setted) for community_setted in communities_setted]
 
-            # print('comunidades finalmente',new_random_partition)
+        # print('comunidades finalmente',new_random_partition)
 
         return new_random_partition
-
     
-    
-    return change_to_random_community(s_iter,node)
+    return random.choice([change_to_random_community,create_new_community])(s_iter,node)
 
 def explore_neighborhood(network, s_iter, s_best, tabu_moves, s_neigh, node_best):
-    
+    print('BEST SOLUTION QUE ACABA DE ENTRAR A EXPLORE NEIGHBORHOOD',s_best)
     node_best = 0
     for node in network.nodes:
+        print('BEST SOLUTION justo antes de  solution from move',s_best)
         s_move = solution_from_move(network, s_iter, node) #¿necesario introducir network?, creo que no (y de momento no se usa)
+        ## Añadido por mi 
+        ##
+        print('BEST SOLUTION tras solution from move',s_best)
+
         if mymodularity(network,s_move) > mymodularity(network,s_best):
             tabu_moves[node] = 0
         
         if tabu_moves[node] == 0 and (node_best == 0 or mymodularity(network,s_move) > mymodularity(network,s_neigh)):
             node_best = node
             s_neigh = s_move
-            
+    
     return tabu_moves, s_neigh, node_best
 
 def tabu_modularity_optimization(network, s_init):
     '''
     Initial_solution = group of nodes (partition of the network). I understand it as different sets of nodes which makes different communities
     '''
-    tabu_tenure = 2 # maximo numero de iteraciones en los que un mismo movimiento no se puede repetir
+    tabu_tenure = 0 # maximo numero de iteraciones en los que un mismo movimiento no se puede repetir
     tabu_moves = [] #contador de movimientos prohibidos, las posiciones son el entero que representa al nodo y el valor es el numero de movimientos restantes para poder volver a moverlo (siendo el maximo, tabu_tenure)
     max_idle = 0 #max numero de iteraciones inactivas, posteriormente lo calculamos en funcion del tamaño del grafo
     num_idle = 0 #numero de iteraciones inactivas (AKA que no implican una mejora)
